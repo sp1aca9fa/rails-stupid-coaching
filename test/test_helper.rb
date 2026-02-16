33b05bb6b9d47d3ffbfdff0953c5284d7fa5774a
Raphael Barbosa <sp1aca9fa@gmail.com>
2026-02-16 13:41:33 +0900

    App running

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,15 +1,25 @@
-ENV["RAILS_ENV"] ||= "test"
-require_relative "../config/environment"
-require "rails/test_help"
+ENV['RAILS_ENV'] ||= 'test'
+require_relative '../config/environment'
+require 'rails/test_help'
 
-module ActiveSupport
-  class TestCase
-    # Run tests in parallel with specified workers
-    parallelize(workers: :number_of_processors)
-
-    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-    fixtures :all
-
-    # Add more helper methods to be used by all tests here...
-  end
+class ActiveSupport::TestCase
+  fixtures :all
 end
+
+Capybara.register_driver :headless_chrome do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+
+  options.add_argument("--headless=new")
+  options.add_argument("--no-sandbox")
+  options.add_argument("--disable-gpu")
+  options.add_argument("--disable-dev-shm-usage")
+  options.add_argument("--window-size=1400,900")
+  options.add_argument("--remote-debugging-port=9222")
+
+  # THE missing piece for WSL
+  options.add_argument("--user-data-dir=#{Dir.home}/chrome-test-profile")
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+end
+Capybara.save_path = Rails.root.join('tmp/capybara')
+Capybara.javascript_driver = :headless_chrome
